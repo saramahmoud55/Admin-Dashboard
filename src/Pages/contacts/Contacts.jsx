a109b45cd4a10dd932385c4a878a34e4b7926d7d
sara <saraelsadek55555@gmail.com>
2024-08-08 17:46:58 +0300

    create contacts page

--- src/Pages/contacts/Contacts.jsx
+++ src/Pages/contacts/Contacts.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import { Box } from '@mui/material'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid'
+import { rows,columns } from './data'
 
 function Contacts() {
   return (
-    <div>
-      contacts page
-    </div>
+    <Box sx={{ height: 600, width: "100%" ,mx:"auto"}}>
+    <DataGrid
+     slots={{
+      toolbar: GridToolbar,
+    }}
+      rows={rows}
+      // @ts-ignore
+      columns={columns}
+    />
+  </Box>
   )
 }
 
--- src/Pages/invoices/Invoices.jsx
+++ src/Pages/invoices/Invoices.jsx
@@ -1,10 +1,19 @@
+import { Box } from '@mui/material'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid'
 import React from 'react'
 
 function Invoices() {
   return (
-    <div>
-      invo page
-    </div>
+    <Box sx={{ height: 600, width: "100%" ,mx:"auto"}}>
+    <DataGrid
+     slots={{
+      toolbar: GridToolbar,
+    }}
+      rows={rows}
+      // @ts-ignore
+      columns={columns}
+    />
+  </Box>
   )
 }
 
