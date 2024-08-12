eb6f07127b6a94ff3fb6a1f8a844d297caa2124a
sara <saraelsadek55555@gmail.com>
2024-08-12 15:52:10 +0300

    creating invoices page

--- src/Pages/invoices/Invoices.jsx
+++ src/Pages/invoices/Invoices.jsx
@@ -1,15 +1,16 @@
 import { Box } from '@mui/material'
 import { DataGrid, GridToolbar } from '@mui/x-data-grid'
-import React from 'react'
+import { columns, rows } from './data'
 
 function Invoices() {
   return (
     <Box sx={{ height: 600, width: "100%" ,mx:"auto"}}>
     <DataGrid
+    checkboxSelection
      slots={{
       toolbar: GridToolbar,
     }}
-      // rows={rows}
+      rows={rows}
       // @ts-ignore
       columns={columns}
     />
