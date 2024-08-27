bd59b784b9d9ce53fd3029d0cbd4bd285b5ed1b8
sara <saraelsadek55555@gmail.com>
2024-08-27 16:51:42 +0300

    creating form page

--- src/Pages/form/Form.jsx
+++ src/Pages/form/Form.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import { Stack } from "@mui/material";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 function Form() {
   return (
-    <div>
-      form page bjlnlkjn
-    </div>
-  )
+    <Box
+      component="form"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 3,
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <Stack direction={"row"} sx={{gap:2}}>
+        <TextField sx={{flex:1}} label="First Name" variant="filled" />
+        <TextField sx={{flex:1}} label="Last Name" variant="filled" />
+      </Stack>
+      <TextField  label="Email" variant="filled" />
+      <TextField  label="Contact Number" variant="filled" />
+      <TextField  label="Address 1" variant="filled" />
+      <TextField  label="Address 2" variant="filled" />
+    </Box>
+  );
 }
 
-export default Form
+export default Form;
