8dc6bf5cfcf7248b37bf3678d8cd32654f87eaa8
sara <saraelsadek55555@gmail.com>
2024-09-02 16:14:57 +0300

    new user button

--- src/Pages/form/Form.jsx
+++ src/Pages/form/Form.jsx
@@ -1,7 +1,24 @@
-import { Stack } from "@mui/material";
+import { Button, MenuItem, Stack } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 
+
+
+const data = [
+  {
+    value: 'Admin',
+    label: 'Admin',
+  },
+  {
+    value: 'Manager',
+    label: 'Manager',
+  },
+  {
+    value: 'User',
+    label: 'User',
+  },
+
+];
 function Form() {
   return (
     <Box
@@ -21,7 +38,28 @@ function Form() {
       <TextField  label="Email" variant="filled" />
       <TextField  label="Contact Number" variant="filled" />
       <TextField  label="Address 1" variant="filled" />
-      {/* <TextField  label="Address 2" variant="filled" /> */}
+      <TextField  label="Address 2" variant="filled" />
+
+
+      <TextField
+          id="outlined-select-currency"
+          select
+          label="Role"
+          defaultValue="User"
+          variant="filled"
+        >
+          {data.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <Box sx={{textAlign:"right"}}>
+        <Button type="submit" sx={{textTransform:"capitalize"}} variant="contained" >
+          Create New User
+        </Button>
+
+        </Box>
     </Box>
   );
 }
