216d8df6016be477a5d3f354a9c20ec2b41c80ed
sara <saraelsadek55555@gmail.com>
2024-09-02 17:39:37 +0300

    new user button

--- src/Pages/form/Form.jsx
+++ src/Pages/form/Form.jsx
@@ -1,27 +1,34 @@
 import { Button, MenuItem, Stack } from "@mui/material";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-
-
+import { useForm } from "react-hook-form";
 
 const data = [
   {
-    value: 'Admin',
-    label: 'Admin',
+    value: "Admin",
+    label: "Admin",
   },
   {
-    value: 'Manager',
-    label: 'Manager',
+    value: "Manager",
+    label: "Manager",
   },
   {
-    value: 'User',
-    label: 'User',
+    value: "User",
+    label: "User",
   },
-
 ];
 function Form() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = () => console.log("done");
   return (
     <Box
+      onSubmit={handleSubmit(onSubmit)}
       component="form"
       sx={{
         display: "flex",
@@ -31,35 +38,67 @@ function Form() {
       noValidate
       autoComplete="off"
     >
-      <Stack direction={"row"} sx={{gap:2}}>
-        <TextField sx={{flex:1}} label="First Name" variant="filled" />
-        <TextField sx={{flex:1}} label="Last Name" variant="filled" />
+      <Stack direction={"row"} sx={{ gap: 2 }}>
+        <TextField
+            error={Boolean(errors.firstName)}
+          helperText={
+            Boolean(errors.firstName) ? "This filed is required" : null
+          }
+          {...register("firstName", { required: true, minLength: 3 })}
+          sx={{ flex: 1 }}
+          label="First Name"
+          variant="filled"
+        />
+        <TextField
+          helperText={
+            Boolean(errors.lastName) ? "This filed is required" : null
+          }
+          {...register("lastName", { required: true, minLength: 3 })}
+          error={Boolean(errors.lastName)}
+          sx={{ flex: 1 }}
+          label="Last Name"
+          variant="filled"
+        />
       </Stack>
-      <TextField  label="Email" variant="filled" />
-      <TextField  label="Contact Number" variant="filled" />
-      <TextField  label="Address 1" variant="filled" />
-      <TextField  label="Address 2" variant="filled" />
-
+      <TextField
+        error={false}
+        label="Email"
+        variant="filled"
+        helperText="This filed is required"
+        {...register("firstName")}
+      />
+      <TextField
+        error={false}
+        label="Contact Number"
+        variant="filled"
+        helperText="This filed is required"
+        {...register("firstName")}
+      />
+      <TextField label="Address 1" variant="filled" />
+      <TextField label="Address 2" variant="filled" />
 
       <TextField
-          id="outlined-select-currency"
-          select
-          label="Role"
-          defaultValue="User"
-          variant="filled"
+        id="outlined-select-currency"
+        select
+        label="Role"
+        defaultValue="User"
+        variant="filled"
+      >
+        {data.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        ))}
+      </TextField>
+      <Box sx={{ textAlign: "right" }}>
+        <Button
+          type="submit"
+          sx={{ textTransform: "capitalize" }}
+          variant="contained"
         >
-          {data.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        <Box sx={{textAlign:"right"}}>
-        <Button type="submit" sx={{textTransform:"capitalize"}} variant="contained" >
           Create New User
         </Button>
-
-        </Box>
+      </Box>
     </Box>
   );
 }
